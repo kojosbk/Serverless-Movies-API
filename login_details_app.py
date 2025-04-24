96e185665b8020f5b8883c939c73646044fa3b08
Silas Kojo Bortey <sila.bortey@gmail.com>
2025-04-24 16:01:47 +0100

    Update login_details_app.py
    update to password

--- login_details_app.py
+++ login_details_app.py
@@ -48,7 +48,7 @@ def generate_message(data):
 
     username = f"{first_name.lower()}.{last_name.lower()}"
     default_password = generate_password_from_name(first_name, last_name)
-    spectra_pm_password = "****REDACTED****"
+    spectra_pm_password = SPECTRA_DEFAULT_PASSWORD
 
     manager_parts = manager_name.split()
     manager_username = f"{manager_parts[0].lower()}.{manager_parts[-1].lower()}" if len(manager_parts) >= 2 else manager_parts[0].lower()
